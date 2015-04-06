eecd0003c7b75253d78bbcbf789641f36e8cfda0
Zach Mueller <zach.a.mueller@gmail.com>
2015-04-06 16:11:36 -0500

    1.0 ~~ Basic Hello World

---
+++ blog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render_to_response
+
+from models import posts
+
+
+def home(request):
+    return render_to_response('index.html')
+
+
+
+# Create your views here.
+
+
